78317892f511a274a5e52426b17e884f71b99044
Kyle Thousand <kylethousand@kyles-mbp-2.lan>
2020-10-19 20:50:06 -0400

    Kyle Thousand

--- generatePhoneNumber.js
+++ generatePhoneNumber.js
@@ -1,8 +1,19 @@
 // GENERATE PHONE NUMBER
+// nums = [3, 2, 1, 2, 3, 4, 5, 4, 5, 4];
+nums = 8885653421;
 
-const generatePhoneNumber = nums => {
+const generatePhoneNumber = (nums) => {
+  if (Array.isArray(nums) === true) {
+    nums = nums.join("");
+  }
+  let string = nums.toString();
+  let phone = `${string.slice(0, 3)}-${string.slice(3, 6)}-${string.slice(
+    6,
+    10
+  )}`;
+  return phone;
+};
 
+console.log(generatePhoneNumber(nums));
 
-}
-
-module.exports = generatePhoneNumber
+module.exports = generatePhoneNumber;
